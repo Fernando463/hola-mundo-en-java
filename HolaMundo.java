665bc940ea7e06877471a8a037770f285d237076
Thorium <facturas5454@gmail.com>
2021-03-10 16:28:09 +0100

    Saludo ampliado

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,10 +1,12 @@
 /**
 * Muestra por pantalla la frase Hola Mundo
+*
 * @uthor ronin
 */
 
 public class HolaMundo {
 	public static void main(String[]args) {
-	System.out.print("Hola Mundo");
+	System.out.println("Hola Mundo");
+	System.out.println("Aquí estamos haciendo pruebas con Git");
 	}
 }
